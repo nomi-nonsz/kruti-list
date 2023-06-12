fe5ea6c8ee2da21db1f9769cbda16247470d47e4
norman-andrians <norman25.na@gmail.com>
2023-06-13 00:27:18 +0700

    add base components i guess

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import React from "react";
+import './assets/css/dist/output.css';
 
 function App() {
-  return
+  return <>
+    <main className="py-5">
+      <header className="text-center">
+        <h1 className="text-3xl font-bold">Kruti List</h1>
+        <p className="mt-1 mx-auto w-3/4">Kruti list is a free and open-source to-do list app that does something</p>
+      </header>
+    </main>
+  </>
 }
 
 export default App;
