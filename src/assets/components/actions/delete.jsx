bc0696c65e0c390f344871df5313918408a4f969
norman-andrians <norman25.na@gmail.com>
2023-09-11 09:07:24 +0700

    seperate to the client and server

--- src/assets/components/actions/delete.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function DeleteIcon(props) {
-    return <button className="text-rose-600" onClick={props.onClick}>
-        <i class="bi bi-trash3-fill"></i>
-    </button>
-}
-
-export default DeleteIcon;
--- src/assets/components/actions/edit.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function EditIcon(prop) {
-    return <button onClick={prop.onClick}>
-        <i class="bi bi-pencil-fill text-blue-500 text-sm"></i>
-    </button>
-}
-
-export default EditIcon;
